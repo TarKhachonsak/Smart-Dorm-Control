445d3ff61bc566836eba33b2be939f96996f0cab
TarKhachonsak <khachonsak117@gmail.com>
2024-02-29 17:02:12 +0700

    add

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
     <Navbar/>
-    <Tanantinformation/>
+    <Contactus/>
     <Footer/>
 </template>
---
+++ components/Contactus.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex justify-center my-20">
+    <div class="mx-10">
+        <img src="../public/me.png" alt="">
+    </div>
+    <div class="my-10 font-bold text-2xl">
+        <div class="flex my-6">
+            <div>
+                <i class="fa-brands fa-facebook"></i>
+            </div>
+            <div>
+                <p> : https://www.facebook.com/Zlow.ei</p>
+            </div>
+        </div>
+        <div class="flex my-6">
+            <div>
+                <i class="fa-brands fa-square-instagram"></i>
+            </div>
+            <div>
+                <p>: https://www.instagram.com/n.nsen?</p>
+            </div>
+        </div>
+        <div class="flex my-6">
+            <div>
+                <i class="fa-brands fa-square-x-twitter"></i>
+            </div>
+            <div>
+                <p> : https://twitter.com/_Zlow_</p>
+            </div>
+        </div>
+        <div>
+            <p>TEL . 097-874-4459</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
